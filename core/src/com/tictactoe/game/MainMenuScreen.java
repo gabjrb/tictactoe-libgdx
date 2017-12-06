135bad35f131823d2f292ced6200f44d2f44a9c3
gabjrb <gab.rued@gmail.com>
2017-12-06 09:34:30 -0300

    TicTacToeScreen (WIP)

--- core/src/com/tictactoe/game/MainMenuScreen.java
+++ core/src/com/tictactoe/game/MainMenuScreen.java
@@ -15,6 +15,7 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -22,6 +23,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -35,13 +37,16 @@ import javax.swing.text.View;
  */
 
 public class MainMenuScreen extends InputAdapter implements Screen {
-    private TictactoeGame game;
+    TictactoeGame game;
     private Stage stage;
     private TextureAtlas atlas;
-    private Table table;
+    private Table rootTable;
+    private Table menuTable;
+    private Table footerButtonsTable;
     private Skin skin;
-    private TextButton buttonPlay, buttonSetting, buttonExit;
-    private Label heading;
+    private TextButton btnPlayOne, btnPlayTwo, btnOptions, btnExit;
+    private ImageButton btnShare, btnRate;
+    private Label lblTitle;
     private FreeTypeFontGenerator fontGenerator;
     private Camera camera;
     private Viewport viewport;
@@ -64,12 +69,11 @@ public class MainMenuScreen extends InputAdapter implements Screen {
         Gdx.input.setInputProcessor(stage);
         atlas = new TextureAtlas("ui/TicTacToe.atlas");
         skin = new Skin(Gdx.files.internal("ui/tictactoe-ui.json"), atlas);
-        table = new Table(skin);
-        table.setFillParent(true);
-
-        //table.debug();
-        stage.addActor(table);
-        table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        rootTable = new Table();
+        rootTable.setDebug(true);
+        rootTable.setFillParent(true);
+        rootTable.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        stage.addActor(rootTable);
 
         fontButtonGenerator = new FreeTypeFontGenerator(Gdx.files.internal("font/OpenSans-Semibold.ttf"));
 
@@ -84,39 +88,53 @@ public class MainMenuScreen extends InputAdapter implements Screen {
         NinePatchDrawable ninePatchDrawableUP = new NinePatchDrawable(patch);
 
         TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
-
-        //        TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
         buttonStyle.font = fontButtonGenerator.generateFont(fontButtonParameter);
-
-        buttonStyle.up = ninePatchDrawableUP;//skin.getDrawable("button");
+        buttonStyle.up = ninePatchDrawableUP;
         buttonStyle.down = skin.getDrawable("button-pressed");
 
-        buttonPlay = new TextButton("Play", buttonStyle);
-        buttonPlay.setColor(new Color(0,0.35f,1,1));
-
-        //buttonPlay.getLabel().setFontScale(1.5f);
-
-        buttonPlay.addListener(new ClickListener() {
+        //
+        // One Player
+        //
+        btnPlayOne = new TextButton("One Player", buttonStyle);
+        btnPlayOne.setColor(new Color(0,0.35f,1,1));
+        btnPlayOne.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new TicTacToeScreen(game));
             }
         });
-        buttonSetting = new TextButton("Settings", buttonStyle);
-        buttonSetting.setColor(new Color(0,0.35f,1,1));
-        //buttonSetting.getLabel().setFontScale(1.5f);
-        buttonExit = new TextButton("Exit", buttonStyle);
-        buttonExit.setColor(new Color(0,0.35f,1,1));
-        //buttonExit.getLabel().setFontScale(1.5f);
-        buttonExit.addListener(new ClickListener() {
+        //
+        // Two Player
+        //
+        btnPlayTwo = new TextButton("Two Player", buttonStyle);
+        btnPlayTwo.setColor(new Color(0,0.35f,1,1));
+        btnPlayTwo.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new TicTacToeScreen(game));
+            }
+        });
+        //
+        // Options
+        //
+        btnOptions = new TextButton("Options", buttonStyle);
+        btnOptions.setColor(new Color(0,0.35f,1,1));
+        //
+        // Exit
+        //
+        btnExit = new TextButton("Exit", buttonStyle);
+        btnExit.setColor(new Color(0,0.35f,1,1));
+        btnExit.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 Gdx.app.exit();
             }
         });
-        buttonExit.pad(15);
-        buttonSetting.pad(15);
-        buttonPlay.pad(15);
+
+        btnPlayOne.pad(15);
+        btnPlayTwo.pad(15);
+        btnOptions.pad(15);
+        btnExit.pad(15);
 
         fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("font/OpenSans-Bold.ttf"));
 
@@ -130,27 +148,54 @@ public class MainMenuScreen extends InputAdapter implements Screen {
 
         fontParameter.magFilter = Texture.TextureFilter.MipMapLinearNearest;
 
-        Label.LabelStyle styleLbl = new Label.LabelStyle(fontGenerator.generateFont(fontParameter), Color.WHITE);
+        Label.LabelStyle styleLbl = new Label.LabelStyle(fontGenerator.generateFont(fontParameter), Color.BLACK);
+        lblTitle = new Label("TicTacToe", styleLbl);
 
-        heading = new Label("TicTacToe", styleLbl);
 
-                //OpenSans-Light
+        menuTable = new Table();
+        menuTable.add(btnPlayOne).height(100).width(300).padBottom(30);
+        menuTable.row();
+        menuTable.add(btnPlayTwo).height(100).width(300).padBottom(30);
+        menuTable.row();
+        menuTable.add(btnOptions).height(100).width(300).padBottom(30);
+        menuTable.row();
+        menuTable.add(btnExit).height(100).width(300);
 
-//        heading = new Label("TicTacToe", skin);
-//        heading.setFontScale(3);
-        heading.setAlignment(Align.top);
-        table.add(heading).height(200).padBottom(30);
-        table.row();
-        table.add(buttonPlay).width(300).height(100).padBottom(30);
-        table.row();
-        table.add(buttonSetting).width(300).height(100).padBottom(30);
-        table.row();
-        table.add(buttonExit).width(300).height(100).padBottom(30);
+        footerButtonsTable = new Table();
+
+        btnRate = new ImageButton(skin.getDrawable("rate-game"));
+        btnShare = new ImageButton(skin.getDrawable("share-game"));
+
+        btnRate.setWidth(75);
+        btnRate.setHeight(75);
+        btnRate.getImageCell().width(75);
+        btnRate.getImageCell().height(75);
+        btnRate.getImage().setScaling(Scaling.stretch);
+
+        btnShare.setWidth(75);
+        btnShare.setHeight(75);
+        btnShare.getImageCell().width(75);
+        btnShare.getImageCell().height(75);
+        btnShare.getImage().setScaling(Scaling.stretch);
+
+        footerButtonsTable.add(btnShare).expand();
+        footerButtonsTable.add().width(150);
+        footerButtonsTable.add(btnRate).expand();
+
+        rootTable.add(lblTitle).height(200);
+        rootTable.row();
+        rootTable.add(menuTable).expand();
+        rootTable.row();
+        rootTable.add(footerButtonsTable);
+        rootTable.row();
+        rootTable.add().height(150);
+
+
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         viewport.apply();
         stage.getViewport().apply();
@@ -163,7 +208,7 @@ public class MainMenuScreen extends InputAdapter implements Screen {
     public void resize(int width, int height) {
         viewport.update(width, height, true);
         stage.getViewport().update(width, height,true);
-        table.invalidateHierarchy();
+//        table.invalidateHierarchy();
     }
 
     @Override
